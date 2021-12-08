3ac517459e19b965487aaf7775d2248696b78367
Brook <71164623+mrbrooklyn@users.noreply.github.com>
2021-12-08 12:01:41 +0700

    Add files via upload

---
+++ 12-8-2021/App.js
@@ -0,0 +1,23 @@
+//rnfes
+
+import React from 'react';
+import TextInputReview from './components/TextInputReview';
+import ImageWithTextInput from './components/ImageWithTextInput';
+import ButtonExample from './components/ButtonExample';
+import TextInputPractice1 from './components/TextInputPractice1'
+import TouchableExample from './components/TouchableExample'
+import TouchablePractice from './components/TouchablePractice'
+
+const App = () => {
+  return(
+    //<TextInputReview/>
+    //<ImageWithTextInput/>
+    //<ButtonExample/>
+    //<TextInputPractice1/>
+    //<TouchableExample/>
+    <TouchablePractice/>
+  );
+};
+
+export default App;
+
